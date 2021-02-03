584488c946050341660ff4e351d251091bf45512
Stijn van Loon <stijnvanloon@home.nl>
2021-02-03 17:07:24 +0100

    f1 tests

--- testapp-moviecasus/Tests/F1Tests.cs
+++ testapp-moviecasus/Tests/F1Tests.cs
@@ -1,6 +1,7 @@
 using Core.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System;
 
 namespace Tests
 {
@@ -10,19 +11,89 @@ namespace Tests
         [TestMethod]
         public void SecondTicketFreeIfStudent()
         {
+            //arrange
             Order order = new Order(1, true);
 
-            //MovieTicket ticket1 = new MovieTicket();
+            MovieTicket ticket1 = new MovieTicket(getMovies()[0], false, 5, 28);
+            MovieTicket ticket2 = new MovieTicket(getMovies()[0], false, 5, 28);
 
-            double price = order.CalculatePrice();
+            order.AddSeatReservation(ticket1);
+            order.AddSeatReservation(ticket2);
+
+            //act
+            double singleTicketPrice = ticket1.GetPrice();
+            double orderPrice = order.CalculatePrice();
+
+            //assert
+            Assert.AreEqual(singleTicketPrice, orderPrice);
+        }
+        [TestMethod]
+        public void SecondsTicketFreeIfMidWeekIfNotStudent()
+        {
+            //arrange
+            Order order = new Order(1, false);
+
+            MovieTicket ticket1 = new MovieTicket(getMovies()[0], false, 5, 28);
+            MovieTicket ticket2 = new MovieTicket(getMovies()[0], false, 5, 28);
+
+            order.AddSeatReservation(ticket1);
+            order.AddSeatReservation(ticket2);
+
+            //act
+            double singleTicketPrice = ticket1.GetPrice();
+            double orderPrice = order.CalculatePrice();
+
+            //assert
+            Assert.AreEqual(singleTicketPrice, orderPrice);
         }
 
-/*        public ICollection<MovieScreening> getMovies()
+        [TestMethod]
+        public void SecondsTicketNotFreeIfWeekendIfNotStudent()
         {
-            ICollection<MovieScreening> collection = new List<MovieScreening>();
+            //arrange
+            Order order = new Order(1, false);
+
+            MovieTicket ticket1 = new MovieTicket(getMovies()[1], false, 5, 28);
+            MovieTicket ticket2 = new MovieTicket(getMovies()[1], false, 5, 28);
+
+            order.AddSeatReservation(ticket1);
+            order.AddSeatReservation(ticket2);
+
+            //act
+            double targetPrice = ticket1.GetPrice() + ticket2.GetPrice();
+            double orderPrice = order.CalculatePrice();
+
+            //assert
+            Assert.AreEqual(targetPrice, orderPrice);
+        }
+
+        public List<MovieScreening> getMovies()
+        {
+            List<MovieScreening> collection = new List<MovieScreening>();
+
+            //midweek
+            collection.Add(new MovieScreening(
+                new Movie("Henk en de UberCharge"),
+                DateTime.Parse("Jan 1, 2009"),
+                14.0
+            ));
+
+            //in weekend
+            collection.Add(new MovieScreening(
+                new Movie("Kaas is een Zoogdier"),
+                DateTime.Parse("Feb 6, 2021"),
+                14.0
+            ));
+
+            //midweek
+            collection.Add(new MovieScreening(
+                new Movie("Namaak kaas"),
+                DateTime.Parse("Jan 1, 2009"),
+                14.0
+            ));
 
 
-            return new ICollection<>()
-        }*/
+            return collection;
+        }
     }
 }
