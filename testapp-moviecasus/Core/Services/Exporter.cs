8d78ebbda9ce4141b79800f17026a3ebd479e53d
Jay Hagendoorn <jsh_wer@hotmail.com>
2021-02-04 12:03:44 +0100

    Revert "Commented out export"
    This reverts commit 71c88795333f3025d9186080d39e7a8832242842.

--- testapp-moviecasus/Core/Services/Exporter.cs
+++ testapp-moviecasus/Core/Services/Exporter.cs
@@ -9,21 +9,21 @@ namespace Core.Services
     {
         public static void ExportToJson(Order order)
         {
-            // using StreamWriter file = new StreamWriter($@"\Order{order.GetOrderNr()}.json");
-            // JsonSerializer serializer = new JsonSerializer {Formatting = Formatting.Indented};
-            // serializer.Serialize(file, order.Tickets);
+            using StreamWriter file = new StreamWriter($@"\Order{order.GetOrderNr()}.json");
+            JsonSerializer serializer = new JsonSerializer {Formatting = Formatting.Indented};
+            serializer.Serialize(file, order.Tickets);
         }
 
         public static void ExportToText(Order order)
         {
-            // StringBuilder builder = new StringBuilder();
-            // foreach (MovieTicket ticket in order.Tickets)
-            // {
-            //     builder.AppendLine(ticket.ToString());
-            // }
-            //
-            // using StreamWriter file = new StreamWriter($@"\Order{order.GetOrderNr()}.txt");
-            // file.Write(builder.ToString());
+            StringBuilder builder = new StringBuilder();
+            foreach (MovieTicket ticket in order.Tickets)
+            {
+                builder.AppendLine(ticket.ToString());
+            }
+
+            using StreamWriter file = new StreamWriter($@"\Order{order.GetOrderNr()}.txt");
+            file.Write(builder.ToString());
         }
     }
 }
